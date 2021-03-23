8e6026fc7538552a2d3b056c011f1d2b054a9719
Pudderz <mpudney6@gmail.com>
2021-03-23 14:24:31 +0000

    Added dispatch, replaceReducer and subscribe functionss

--- redux/src/redux/createStore.js
+++ redux/src/redux/createStore.js
@@ -24,15 +24,62 @@ export const createStore = (reducer, preloadedState)=>{
 
 
     const subscribe = (listener)=>{
+        if(typeof listener !== "function"){
+            throw new Error("Listener has to be function")
+        }
+        if(isDispatching) throw new Error("cannot subscribe while dispatching")
+
+        let isSubscribed = true;
+
+
+        nextListeners.push(listener);
+
+        return function unsubscribe(){
+            if(!isSubscribed) return
+
+            if(isDispatching)throw new Error("cannot unsubscribe while dispatching")
+
+            isSubscribed = false;
+
+            const index = nextListeners.indexOf(listener);
+            nextListeners.splice(index, 1);
+            currentListeners = null;
+        }
     }
 
 
 
     const dispatch=(action)=>{
+
+        // runs dispatcher function with currentState and action as arguments and will return a new
+        // state
+
+        try{
+            isDispatching = true;
+            currentState = currentReducer(currentState, action);
+        } finally {
+            isDispatching = false;
+        }
+
+
+        //Runs all listener functions after a dispatch
+
+        const listeners = currentListeners;
+        for(let i = 0; i< listeners.length; i++){
+            const listener = listeners[i];
+            listener();
+        }
+
+        return action;
     }
 
 
     const replaceReducer = (reducer)=>{
+        if(isDispatching) throw new Error("cannot replace reducer during dispatch")
+        if(typeof reducer !== "function") throw new Error("Reducer has to be a function")
+
+        currentReducer = reducer;
+        
     }
 
     const store = {
