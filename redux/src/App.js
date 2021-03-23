679f260fd73c6cab4e803451c1000422e2571dd1
Pudderz <mpudney6@gmail.com>
2021-03-23 18:20:43 +0000

    created store example in app.js

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createStore } from "./redux/createStore";
 
 function App() {
+  const reducer = (state, action) => {
+    if (action.type === "INC") {
+      return { ...state, number: state.number + action.payload };
+    }
 
-  
+    if (action.type === "DEC") {
+      return { ...state, number: state.number - action.payload };
+    }
+    return { state };
+  };
 
+  const store = createStore(reducer, { number: 0 });
 
+  const listener = () => console.log(store.getState());
+  store.subscribe(listener);
+
+  store.dispatch({ type: "INC", payload: 5 });
+  store.dispatch({ type: "INC", payload: 2 });
+  store.dispatch({ type: "DEC", payload: 7 });
 
   return (
     <div className="App">
